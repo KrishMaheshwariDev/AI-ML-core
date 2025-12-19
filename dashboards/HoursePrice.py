e1328e9cb661a6430d4179b7de109d98175d565a
KrishMaheshwariDev <krishmaheshwari197903@gmail.com>
2025-12-20 01:19:57 +0530

    Refactored and Improved the HoursePrice Dashboard

--- dashboards/HoursePrice.py
+++ dashboards/HoursePrice.py
@@ -2,9 +2,9 @@ import os
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import statsmodels.api as sm
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import statsmodels.api as sm
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
 # Absolute path resolution
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -22,58 +22,99 @@ def load_data():
 
 df = load_data()
 
-# Dataset overview
-st.header("Dataset Overview")
-st.write(df.head())
-st.write(f"Shape: {df.shape}")
+# SideBar Panel
+st.sidebar.header("EDA controls")
+target = st.sidebar.selectbox("Select the Target Column", options=df.columns)
 
-# SalePrice distribution
-st.header("SalePrice Distribution")
-fig = px.histogram(df, x="SalePrice", nbins=50, marginal="box")
-st.plotly_chart(fig, use_container_width=True)
+# Variables
 
-# Numerical Fetures relation with SalesPrice
-st.header("Numerical Features vs SalesPrice")
+num_features = []
+cat_features = []
 
-numerical_features = df.select_dtypes(include=['int64','float64']).columns
+if target in df.select_dtypes(include=["object"]).columns:
+    num_features = df.select_dtypes(include=["int64", "float64"]).columns.drop('Id')
+    cat_features = df.select_dtypes(include="object").columns.drop(target)
+else:
+    num_features = df.select_dtypes(include=["int64", "float64"]).columns.drop([target, "Id"])
+    cat_features = df.select_dtypes(include="object").columns
 
-feature = st.selectbox("Select the numerical feture", numerical_features)
+# Tabs Section
 
-fig = px.scatter(
-    df,
-    x=feature,
-    y="SalePrice",
-    trendline="ols",
-    opacity=0.5,
-    title=f"{feature} vs SalesPrice"
-)
+tab1, tab2, tab3, tab4 = st.tabs([
+    "Overview",
+    "Distributions",
+    "Relationships",
+    "Correlation & Risk"
+])
 
-st.plotly_chart(fig, use_container_width=True)
+# Tab1 : Overview
+with tab1:
+    st.header("Dataset Overview")
+    st.write(df.head(10))
+    st.write(f"Shape: {df.shape}")
 
-fig = px.bar(
-    df,
-    x=feature,
-    y="SalePrice",
-    opacity=1,
-    title=f"{feature} vs SalesPrice"
-)
+# Tab2 : Distributions
+with tab2:
+    feature = st.selectbox("Select the Numerical Feature", num_features)
 
-st.plotly_chart(fig, use_container_width=True)
+    col1, col2 = st.columns([3,1])
 
-st.header("Correlation with SalePrice")
+    with col1:
+        fig = px.histogram(
+            df,
+            x=feature,
+            nbins=50,
+            title=f"Distribution of {feature}"
+        )
 
-fig = px.imshow(
-    df.corr(numeric_only=True),
-    x=numerical_features,
-    y=numerical_features
-)
+        st.plotly_chart(fig, use_container_width=True)
 
-st.plotly_chart(fig, use_container_width=True)
+    with col2:
+        fig = px.box(
+            df,
+            y=feature,
+            title=f"Outliers of {feature}"
+        )
 
-corr = (
-    df.select_dtypes(include=["int64", "float64"])
-      .corr()["SalePrice"]
-      .sort_values(ascending=False)
-)
+        st.plotly_chart(fig, use_container_width=True)
 
-st.dataframe(corr)
+# Tab 3: Relationships
+
+with tab3:
+    feature = st.selectbox("Select the Feature", num_features)
+
+    fig = px.scatter(
+        df,
+        x=feature,
+        y=target,
+        trendline="ols",
+        opacity=0.5,
+        title=f"{feature} vs {target}"
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+# Tab 4: correlations
+
+with tab4:
+    st.header("Top Correlated Features")
+
+    k = st.slider("Top K features", 5, 10, 20)
+
+    corr = (
+        df.select_dtypes(include=["int64", "float64"])
+        .corr()[target]
+        .drop(target)
+        .sort_values(ascending=False)
+        .head(k)
+    )
+
+    fig = px.bar(
+        corr,
+        x=corr.values,
+        y=corr.index,
+        orientation="h",
+        title=f"Top {k} Correlated features with {target}"
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
